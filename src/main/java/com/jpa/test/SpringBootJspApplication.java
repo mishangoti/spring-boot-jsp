3bda20d8fcea648cd6f46f912594348d431d0916
mishangoti <mishan2512@gmail.com>
2023-08-08 21:17:01 -0400

    small change

--- src/main/java/com/jpa/test/SpringBootJspApplication.java
+++ src/main/java/com/jpa/test/SpringBootJspApplication.java
@@ -11,6 +11,33 @@ public class SpringBootJspApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJspApplication.class, args);
+//		
+//		UserRepository userRepository = context.getBean(UserRepository.class);
+//		
+//		User user = new User();
+//		user.setName("Goti");
+//		user.setCity("Canada");
+//		user.setStatus("developer");
+//		
+//		User user1 = userRepository.save(user);
+//		System.out.println(user1);
+//		
+//		List<User> allUser = userRepository.getAllUser();
+//		allUser.forEach(e -> {
+//			System.out.println(e);
+//		});
+//		
+//		List<User> userByName = userRepository.getUserByName("Goti");
+//		userByName.forEach(e -> {
+//			System.out.println(e);
+//		});
+//		
+//		List<User> getUsers = userRepository.getUsers();
+//		getUsers.forEach(e -> {
+//			System.out.println(e);
+//		});
+//		System.out.println("test");
+	
 	}
 
 }
